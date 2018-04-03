097998fa97955530376a424b3f3579da413fc836
lee890720 <lee_890720@126.com>
2018-04-04 00:41:01 +0800

    ChangeAlert

--- LeeInfo.Web/ViewComponents/Alert.cs
+++ LeeInfo.Web/ViewComponents/Alert.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LeeInfo.Web.ViewComponents
 {
-    public class Alert:ViewComponent
+    public class Alert : ViewComponent
     {
         private readonly AppDbContext _context;
         public Alert(AppDbContext context)
@@ -20,24 +20,20 @@ namespace LeeInfo.Web.ViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             List<AlertViewModel> list_alert = new List<AlertViewModel>();
-            var ccdData=await _context.CcdData.Where(c => c.RepaymentDate.AddDays(-3) < DateTime.Now).ToListAsync();
-            if(ccdData.Count!=0)
+            var ccdData = await _context.CcdData.Where(c => c.RepaymentDate.AddDays(-3) < DateTime.Now).ToListAsync();
+            if (ccdData.Count != 0)
             {
-                list_alert.Add(new AlertViewModel { Info = "CreditCard Bills", Count = ccdData.Count, Ico = "fa fa-credit-card text-green",Url="/CreditCard/CcdData" });
+                list_alert.Add(new AlertViewModel { Info = "CreditCard Bills", Count = ccdData.Count, Ico = "fa fa-credit-card text-green", Url = "/CreditCard/CcdData" });
             }
             var ccdDebt = await _context.CcdDebt.Where(c => c.RepaymentDate.AddDays(-3) < DateTime.Now).ToListAsync();
-            if(ccdDebt.Count!=0)
+            if (ccdDebt.Count != 0)
             {
-                list_alert.Add(new AlertViewModel { Info = "Debt Bills", Count = ccdDebt.Count, Ico = "fa fa-yen text-yellow" ,Url="/CreditCard/CcdDebt"});
+                list_alert.Add(new AlertViewModel { Info = "Debt Bills", Count = ccdDebt.Count, Ico = "fa fa-yen text-yellow", Url = "/CreditCard/CcdDebt" });
             }
             var frxEcs = await _context.FrxEcs.SingleOrDefaultAsync(f => f.EcsName == "LeeInfo");
-            if(frxEcs.EcsTime.AddMinutes(1)<DateTime.UtcNow)
+            if (frxEcs.EcsTime.AddMinutes(1) < DateTime.UtcNow)
             {
                 list_alert.Add(new AlertViewModel { Info = "The ECS has a mistake", Ico = "fa fa-database text-red", Url = "/" });
-                }
-            else
-            {
-                list_alert.Add(new AlertViewModel { Info = "The ECS is ok", Ico = "fa fa-database text-blue", Url = "/" });
             }
             return View(list_alert);
         }
